9a604f5ac413c1558e34c74e37db2048f170f213
Tyler J <tjamesjob@gmail.com>
2025-05-14 12:38:11 -0700

    moving backto nixos part two

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,45 +1,15 @@
-# Functions that build packages are derivations
-# Installed packages are never changed
-#   This fact means updates in Nix are atomic since there isnt a time when half the system differes from the other
-
-# Default.nix defines how a package is build
-#  This file is translated into a store derivation (more primative)
-
 {
-	description = "Testing Flakes";
-	inputs = {
-		# Use the 23.11 branch of the official package repo
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-		home-manager.url = "github:nix-community/home-manager/release-23.11";
-		# Makesure packages referenced in nixpkgs and home-manager are the same
-		home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  description = "A very basic flake";
 
-        # Neovim
-        #nixvim = {
-          #url = "github:nix-community/nixvim/nixos-23.11";
-          #follows = "nixpkgs";
-        #};
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
 
-	};
-	outputs = { self, nixpkgs, home-manager, ...}@inputs: 
-	let
-		system = "x86_64-linux";
-		pkgs = nixpkgs.legacyPackages.${system};
-	in {
-		nixosConfigurations.hathsin = nixpkgs.lib.nixosSystem {
-			inherit system;
-			# module nix files
-			modules = [
-				./configuration.nix	
-			];
-		};
-		homeConfigurations = {
-			kami = home-manager.lib.homeManagerConfiguration {
-				inherit pkgs;
-                modules = [ 
-                  ./home.nix 
-                ];
-			};
-		};
-	};
+  # self is the directory of this flake in the nix store
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.tranquility = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
+    };
+  };
 }
